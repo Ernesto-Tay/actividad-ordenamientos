b134754da120c401efc9bcca5092ca8786a13270
Santiagobrrrr <sjbatz@correo.url.edu.gt>
2025-10-16 09:11:50 -0600

    bubble y selection sort

---
+++ main.py
@@ -0,0 +1,13 @@
+def bubble_sort(list):
+    for i in range(len(list)-1):
+        for j in range(len(list)-i-1):
+            if list[j] > list[j+1]:
+                list[j], list[j+1] = list[j+1], list[j]
+
+def selection_sort(list):
+    for i in range(len(list)):
+        min_index = i
+        for j in range(i+1, len(list)):
+            if list[j] < list[min_index]:
+                min_index = j
+        list[i], list[min_index] = list[min_index], list[i]
